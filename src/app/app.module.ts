f7495aef33160ee2f54f7f528c42606a9a738e2c
David Ledanseur <david.ledanseur@intech.lu>
2017-10-30 21:31:27 +0100

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './modules/users/components/user-list.component';
 
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: 'full'},
+  { path: "users", component: UserListComponent }
+ ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/users/components/user-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+ templateUrl: './user-list.component.html'
+})
+export class UserListComponent {
+}
